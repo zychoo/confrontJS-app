48807583f337f343db4f2f0dd75af0f81e502dc3
pzych <przemyslaw1.zych@ge.com>
2019-11-28 00:24:27 +0100

    Add imports

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,4 +1,8 @@
 import cdk = require('@aws-cdk/core');
+import s3 = require('@aws-cdk/aws-s3');
+import lambda = require('@aws-cdk/aws-lambda');
+import api = require('@aws-cdk/aws-apigateway');
+import path = require('path');
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
