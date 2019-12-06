6939ba9c00e5179c659d45bca2e87a9d46d483ce
pzych <przemyslaw1.zych@ge.com>
2019-12-06 13:47:39 +0100

    Added comments to my app and lambda

--- back/app.js
+++ back/app.js
@@ -3,10 +3,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+/*
+ * This is node app that is doing some serious business stuff
+ */
 app.get('/api/foo', (req, res, next) => {
 
     //Complicated business logic happens here...
-    const value = 'Hello World from FOO!';
+    const now = new Date();
+    const value = `Hello World from ConFrontJS! <hr> Time is: ${now}`;
    
     res
         .header({ 'Access-Control-Allow-Origin': '*' })
--- back/lambda/index.js
+++ back/lambda/index.js
@@ -1,7 +1,11 @@
+/*
+ * This is lambda that is reflecting our app.js business
+ */
 exports.handler = async (event) => {
 
     //Complicated business logic happens here...
-    const value = 'Hello World from FOO!';
+    const now = new Date().getDate;
+    const value = `Hello World from FOO! Time is: ${now}`;
 
     const response = {
         statusCode: 200,
