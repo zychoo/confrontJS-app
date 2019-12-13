b2ed566b247c1802e821dd882538e8e2e31eb60b
pzych <przemyslaw1.zych@ge.com>
2019-12-13 10:42:04 +0100

    Modified infrastructure stack to final version

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -8,6 +8,22 @@ export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this,"ConFrontJS Bucket", {
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "error.html",
+      publicReadAccess: true
+    });
+
+    const myLambda = new lambda.Function(this, "ConFrontJS Function", {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.asset(path.join(__dirname, "../../back/lambda")),
+      handler: "index.handler"
+    });
+
+    const myApi = new api.RestApi(this, "ConFrontJS API");
+    
+    const integration = new api.LambdaIntegration(myLambda);
+
+    myApi.root.addMethod("GET", integration);
   }
 }
